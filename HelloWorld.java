398e351222d76b940bbb5c9fa6e48e80ab61ecd9
Greg Fury <gregfury@gmail.com>
2017-08-08 14:07:46 -0400

    Begin Lab 5

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/* This is for Lab 5 */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
+
